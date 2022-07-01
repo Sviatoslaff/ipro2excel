ce47be27ff8855626b0ff34a94726c1bdeb2d832
Sviatoslaff <sim504@ya.ru>
2022-07-01 11:31:08 +0300

    Update getgoodsfile.php
    transfer to all Characteristics processing

--- getgoodsfile.php
+++ getgoodsfile.php
@@ -9,42 +9,38 @@ use Box\Spout\Common\Type;
 use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 use Box\Spout\Common\Entity\Row;
 
+$groupChars = array(); 												// словарь характеристик по группам (классам)
+// [XXXX классиф. товара][индекс] = характеристика
+$mainChars = array_fill(0, 20, "reserve");							// плоский словарь характеристик [index] = характеристика
+$articleCol = 5;
+$resArray = array();												// результирующий массив
 
-if (!file_exists("config.ini")) {
+if (!file_exists(__DIR__ ."/" ."config.ini")) {
 	echo "Не обнаружен config.ini -- работа невозможна.";
 	die;
 }
 $ini_array = parse_ini_file("config.ini", true);
-getWeights();
+makeExcelfile();
 
-function getWeights() {
-	global $ini_array;
-	$time1 = date("d-m-Y H:i:s");
-	echo "Старт $time1<br> ";
-	$exp_array = $ini_array["Exporter"];
-	$xlsxname = $exp_array["tempfolder"] . "/" . $exp_array["xlsxgoodsfilename"];
-
-	$etm_array = $ini_array["ETM"]; 
-	$session = getsession($etm_array);
-
-	$in_array = $ini_array["In"]; 
-	$sourcefile = $exp_array["sourcefolder"] . "/" . $in_array["goodssourcefile"];
-
-	if (!file_exists($sourcefile)) {
-		die("<br>Не обнаружен исходный файл $sourcefile -- работа невозможна.");
-	}
+function makeArrays($session, $sourcefile) {
+	global $articleCol, $resArray, $mainChars;	
 	$reader = ReaderEntityFactory::createXLSXReader();
 	$reader = ReaderEntityFactory::createReaderFromFile($sourcefile);
 	$reader->open($sourcefile);
-	$articleCol = 5;
-	$writer = WriterEntityFactory::createXLSXWriter();
-	$writer->setTempFolder($exp_array["tempfolder"]);
-	$writer->openToFile($xlsxname);
-	$notfound = array();
+
 
 	foreach ($reader->getSheetIterator() as $sheet) {
 		foreach ($sheet->getRowIterator() as $rowindex=>$row) {
-			if ($rowindex == 1) { $writer->addRow($row); continue;}			// первую строку просто копируем
+			if ($rowindex == 1) { 											// первую строку просто копируем
+				for ($i = 0; $i < 18; $i++) {
+					$cell = $row->getCellAtIndex($i);
+					if ($cell !== null) 
+						$mainChars[$i] = $row->getCellAtIndex($i)->getValue();
+					else
+						$mainChars[$i] = "";				
+				}				
+				continue;
+			}			
 			if ($row->getCellAtIndex($articleCol) !== null)					// если артикул - не пустой
 				$article = $row->getCellAtIndex($articleCol)->getValue();
 			else
@@ -61,25 +57,75 @@ function getWeights() {
 			if (strpos($article, " ") === false) {							// если нет пробелов в артикуле
 				$status = setGoodsProperties($session, $article, $myrow);
 				if ($status == "200") {
-
+					$resArray[] = $myrow;
 				} else {
 					$notfound[] = $article;
 				}
-				$rowFromValues = WriterEntityFactory::createRowFromArray($myrow);
-				// print_r($myrow);
-				$writer->addRow($rowFromValues);
 			} else {
-				$writer->addRow($row); continue;							// если есть, то вставляем исходную строку
+												// если есть пробелы, то возвращаем исходную строку
 			}
-			// if ($rowindex > 100) break;
+			 if ($rowindex > 100) break;
 		}
 	}
 	$reader->close();
+}
+
+function makeExcelfile() {
+	global $ini_array;
+	global $articleCol, $groupChars, $mainChars, $resArray;	
+	$time1 = date("d-m-Y H:i:s");
+	echo "Старт $time1<br> ";
+	$exp_array = $ini_array["Exporter"];
+	$xlsxname = $exp_array["tempfolder"] . "/" . $exp_array["xlsxgoodsfilename"];
+
+	$etm_array = $ini_array["ETM"]; 
+	$session = getsession($etm_array);
+
+	$in_array = $ini_array["In"]; 
+	$sourcefile = $exp_array["sourcefolder"] . "/" . $in_array["goodssourcefile"];
+
+	if (!file_exists(__DIR__ ."/" .$sourcefile)) {
+		die("<br>Не обнаружен исходный файл $sourcefile -- работа невозможна.");
+	}
+
+	makeArrays($session, $sourcefile);
+
+	// создаем массив с доп.характеристиками
+	foreach ($groupChars as $groupitem) {
+		foreach ($groupitem as $char) {
+			$mainChars[] = $char;
+		}
+	}
+	$qrows = count($mainChars);
+
+	$writer = WriterEntityFactory::createXLSXWriter();
+	$writer->setTempFolder($exp_array["tempfolder"]);
+	$writer->openToFile($xlsxname);
+	$notfound = array();
+
+	// пишем заголовок 
+	$rowFromValues = WriterEntityFactory::createRowFromArray($mainChars);
+	$writer->addRow($rowFromValues);
+
+	foreach ($resArray as $item) {
+		$myrow = array_fill(0, $qrows, "");
+		for ($i = 0; $i < 18; $i++) {
+			$myrow[$i] = $item[$i];
+		}
+		foreach ($item["Chars"] as $char) {
+			if (in_array($char[""], $mainChars)) {
+				$key = array_search($char["gdsCharName"], $mainChars);
+				$myrow[$key] = $char["gdsCharVal"];
+			}
+		}
+		$rowFromValues = WriterEntityFactory::createRowFromArray($myrow);
+		$writer->addRow($rowFromValues);
+	}
 	$writer->close();
 
 	$time2 = date("d-m-Y H:i:s");
 	echo "Финиш $time2<br> ";
-	echo "Обработано $rowindex позиций<br>";
+	echo "Обработано позиций<br>";
 	$countnotfound = count($notfound);
 	echo "Не найдено $countnotfound позиций:<br>";
 	$show = implode(",", $notfound);
@@ -88,12 +134,13 @@ function getWeights() {
 }	
 
 function setGoodsProperties($session, $article, &$row) {
-	global $ini_array;
+	global $ini_array, $groupChars, $mainChars;
 
 	$res_array = makeRequest($article, $session, "");
 	if ($res_array == false) return false;
 	if ($res_array["status"]["code"] == "200") {
 		$arrData = $res_array["data"];
+		$class =  mb_substr($arrData["gdsInfoClass81"], 0 , 2);
 		$row[0] = $arrData["gdsCode"];
 		if (array_key_exists('barcodes',$arrData))
 			$row[7] = strval($arrData["barcodes"][0]["val"]);
@@ -117,6 +164,14 @@ function setGoodsProperties($session, $article, &$row) {
 					break;
 			}
 		}
+
+		if (!array_key_exists($class, $groupChars)) {
+			foreach ($arrMeas as $ch) {
+				$groupChars[$class] = $ch["gdsCharName"];
+				$row["Chars"][] = $ch;
+			}
+		}
+
 	} else {
 		$row[0] = $res_array["status"]["code"] . " " . $res_array["status"]["message"];
 	}
